a29619ad12cff600a8873a9c2a5523a30ea5c21a
Elvric <elvric.trombert@mail.mcgill.ca>
2018-02-17 21:33:44 -0500

    Now the planets can turn

--- Gravity.py
+++ Gravity.py
@@ -1,55 +1,137 @@
 from tkinter import *
+import math
 import time
 
+# Initiate the window and the canvas that will hold the planets
+planets=[]
+root = Tk()
+canvas = Canvas(root,bg='black', width=1000, height=500)
+canvas.pack()
+
+# Object keeping track of the speed vector and mass
 class planet():
     def __init__(self,x,y,sx,sy,m,color):
         self.sx = sx
         self.sy = sy
         self.m = m
-        self.planet = canvas.create_oval(x-m/2,y-m/2,x+m/2,x+m/2,fill=color)
+        self.planet = canvas.create_oval(x-m/2,y-m/2,x+m/2,y+m/2,fill=color)
         self.color=color
 
-    def move(self):
+    def move(self,planets):
         canvas.move(self.planet,self.sx,self.sy)
+        for i in planets:
+            i.force(self)
+
 
     def cord(self):
         dim = canvas.coords(self.planet)
         coord = [dim[2]-self.m/2,dim[3]-self.m/2]
         return coord
-
-    def collide(self,earth):
-        us = self.cord()
-        them = earth.cord()
-        return us[0]==them[0] and them[1]==us[1]
+# Using the law of attraction of the universer
+    def force(self,other_planet):
 
 
+        if self is other_planet:
+            return 0
+
+# Ensures that the gravite and speed vectors are in the right direction
+        if self.m < other_planet.m:
+            other_planet.force(self)
+            return 0
+
+# Get the distance and calculate the mid gravity usin m1*m2/d^2
+        self_location = self.cord()
+        other_location = other_planet.cord()
+        difference = list(map(lambda x1,x2: x1-x2,self_location,other_location))
+        d = 1.0*difference[0]**2+difference[1]**2
+        adjacent = math.sqrt(d)
+        gravity = (self.m*other_planet.m)/d
+
+# Obtain the acceleration vectores
+        ax = difference[0] / adjacent * gravity
+        ay = difference[1] / adjacent * gravity
+
+# Ensures that the acceleration does not get too big
+        if self.m < other_planet.m:
+            self.sx = ax
+            self.sy = ay
+        else:
+            other_planet.sx += ax
+            other_planet.sy += ay
+
+
+        return 0
+
+
+
+
+# ToDO
 def fuse(planet1,planet2):
-    m = planet1.m+planet2.m
     if planet2.m > planet1.m:
         planet1, planet2 = planet2, planet1
-    coord=planet1.cord()
-    newplanet = planet(coord[0],coord[1],planet1.sx,planet1.sy,m,planet1.color)
-    canvas.delete(planet1)
-    canvas.delete(planet2)
-    planets.remove(planet1)
-    planets.remove(planet2)
-    return newplanet
+    planet1.m = planet1.m + planet2.m
+    global canvas
+    canvas.delete(planet2.planet)
+    global planets
+    planets=[planet1]
+
+# Stimulates the word
+def stimulation(planets):
+    while True:
+        for p in planets:
+            p.move(planets)
+        root.update()
+        time.sleep(0.001)
+
+
+def addplanet():
+    nplanet=planet(int(x.get()),int(y.get()),int(sx.get()),int(sy.get()),int(m.get()),color.get())
+    global planets
+    planets.append(nplanet)
+
+# Initialize the sun
+def main():
+    sun = planet(500, 250, 0, 0, 100, "yellow")
+    global planets
+    planets=[sun]
+    # earth = planet(250, 250, 1, 2, 49, "blue")
+    # earth1 = planet(1000, 1000, -3, -5, 20, "red")
+    stimulation(planets)
+canvas1 = Canvas(root,bg='white', width=700, height=500)
+canvas1.pack()
+
+restart = Button(canvas1,text="Restart")
+restart.grid()
+
+addp = Button(canvas1,text="Add Planet", command=addplanet)
+addp.grid(column=1,row=0)
+
+x=Entry(canvas1)
+x.insert(0,"200")
+x.grid(column=0,row=1)
+
+y=Entry(canvas1)
+y.insert(0,"100")
+y.grid(column=1,row=1)
+
+sx=Entry(canvas1)
+sx.insert(0,"1")
+sx.grid(column=0,row=2)
+
+sy=Entry(canvas1)
+sy.grid(column=1,row=2)
+sy.insert(0,"2")
+
+m=Entry(canvas1)
+m.grid(column=0,row=3)
+m.insert(0,"30")
+
+color=Entry(canvas1)
+color.insert(0,"red")
+color.grid(column=1,row=3)
 
 
 
 
-root = Tk()
-canvas = Canvas(root,bg='black', width=500, height=512)
-canvas.pack()
-sun = planet(250,250,-1,-1,100,"yellow")
-sun1 = planet(0,0,1,1,50,"yellow")
-sun2 = canvas.create_oval(10,10,50,50,fill="brown")
-planets= [sun,sun2,sun1]
-while True:
-    sun.move()
-    sun1.move()
-    if sun.collide(sun1):
-        fuse(sun,sun1)
-    root.update()
-    time.sleep(0.1)
-    canvas.delete(sun2)
+
+if __name__== "__main__": main()
